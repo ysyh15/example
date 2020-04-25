ce84c5afb27a76a168a8b5eecabe084d29558897
liuyizhi <liuyz@deepblueai.com>
2020-04-25 21:45:29 +0800

    title: 1.add insertion sort summary: as title test: local

--- algorithm/Sort.go
+++ algorithm/Sort.go
@@ -31,5 +31,30 @@ func bubbleSort(sortObj []int) {
 插入排序
 */
 func insertionSort(sortObj []int) {
+	if len(sortObj) < 1 {
+		return
+	}
+	//只需要从第二个数开始比较
+	for n := 1; n < len(sortObj); n = n + 1 {
+		//记录当前的值
+		value := sortObj[n]
+		//标记前一个位置
+		m := n - 1
+		//从后往前开始遍历比较
+		for ; m >= 0; m-- {
+			//如果当前值小于循环比对的值，
+			if sortObj[m] > value {
+				sortObj[m+1] = sortObj[m]
+				fmt.Println(m)
+			} else {
+				//没有比value小的，就已经标记处当前m的位置了
+				break
+			}
+		}
+		fmt.Println(m)
+		//m的值在经过循环后-1插入数据
+		sortObj[m+1] = value
+	}
+
 	fmt.Println(sortObj)
 }
--- algorithm/main.go
+++ algorithm/main.go
@@ -1,7 +1,7 @@
 package main
 
 func main() {
-	sortObj := []int{1, 6, 4, 2, 5, 2, 222, 43232, 43, 43, 1}
-	bubbleSort(sortObj)
+	sortObj := []int{6, 3, 4,35,67,2,45,6}
+	//bubbleSort(sortObj)
 	insertionSort(sortObj)
 }
