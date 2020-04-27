2e4195c927580330af501e8d1387d33c450f74f8
liuyizhi <liuyz@deepblueai.com>
2020-04-27 21:09:34 +0800

    title: 1.add insertion sort summary: as title test: local

--- algorithm/Sort.go
+++ algorithm/Sort.go
@@ -6,11 +6,14 @@ import "fmt"
 冒泡排序
 */
 func bubbleSort(sortObj []int) {
+	//如果数组只有一个数，则不需要排序
 	if len(sortObj) < 1 {
 		return
 	}
+	//循环需要排序的数据
 	for n := 0; n < len(sortObj)-1; n++ {
 		breakFlag := false
+		//将本次排序的值与所有值进行循环比较
 		for m := 0; m < len(sortObj)-1; m++ {
 			tmp := 0
 			if sortObj[m] > sortObj[m+1] {
@@ -36,25 +39,26 @@ func insertionSort(sortObj []int) {
 	}
 	//只需要从第二个数开始比较
 	for n := 1; n < len(sortObj); n = n + 1 {
-		//记录当前的值
+		//记录当前要插入的值
 		value := sortObj[n]
-		//标记前一个位置
+		//标记当前位置的前一个位置
 		m := n - 1
+		//标记应当插入的地址
+		insertPlace := 0
 		//从后往前开始遍历比较
 		for ; m >= 0; m-- {
-			//如果当前值小于循环比对的值，
-			if sortObj[m] > value {
+			//如果要插入的值小于循环比对的值，
+			if value < sortObj[m] {
+				//将循环比对的值后移一位
 				sortObj[m+1] = sortObj[m]
-				fmt.Println(m)
 			} else {
-				//没有比value小的，就已经标记处当前m的位置了
+				//没有比要插入的值小的，代表上一个循环的值就是要插入的位置，因为上一个循环的值已经被移到后面去了
+				insertPlace = m + 1
 				break
 			}
 		}
-		fmt.Println(m)
-		//m的值在经过循环后-1插入数据
-		sortObj[m+1] = value
+		//将值插入
+		sortObj[insertPlace] = value
 	}
 
-	fmt.Println(sortObj)
 }
